5c89df0d485f15535c6ce7cb14bd8b306455b212
kyhfan <yh.kim@minivertising.kr>
2014-10-14 21:43:43 +0900

    기본 설정 테스트7

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 	include_once "config.php";
 
 
-	$query 		= "SELECT count(*) FROM $_gl[tk_member_table]";
+	$query 		= "SELECT * FROM tk_member";
 	$result 	= mysqli_query($my_db, $query);
 	$data		= mysqli_fetch_array($result);
 	print_r($data);
