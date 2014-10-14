c7b0f6fab59dcaa1b3113869c1a760eea4488900
kyhfan <yh.kim@minivertising.kr>
2014-10-14 21:49:44 +0900

    기본 설정 테스트9

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?
 	include_once "config.php";
-	$my_db = new mysqli("218.54.31.121", "root", "m!nv#Rtisin9", "tomorrowkids");
+	$my_db = new mysqli("localhost", "root", "m!nv#Rtisin9", "tomorrowkids");
 	if (mysqli_connect_error()) {
 		exit('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
 	}
-
 
 	$query 		= "SELECT * FROM tk_member";
 	$result 	= mysqli_query($my_db, $query);
