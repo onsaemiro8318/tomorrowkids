6944098d3fc714bb609d16e3eb39f57fc41cff2c
kyhfan <yh.kim@minivertising.kr>
2014-10-14 21:44:55 +0900

    기본 설정 테스트8

--- include/dbi.php
+++ include/dbi.php
@@ -9,9 +9,10 @@
      *
      ******************************************************************************/
 	
-	//$my_db = new mysqli("218.54.31.121", "root", "m!nv#Rtisin9", "tomorrowkids");
+/*
 	$my_db = new mysqli("218.54.31.121", "root", "m!nv#Rtisin9", "tomorrowkids");
 	if (mysqli_connect_error()) {
 		exit('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
 	}
+*/
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?
 	include_once "config.php";
+	$my_db = new mysqli("218.54.31.121", "root", "m!nv#Rtisin9", "tomorrowkids");
+	if (mysqli_connect_error()) {
+		exit('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
+	}
 
 
 	$query 		= "SELECT * FROM tk_member";
