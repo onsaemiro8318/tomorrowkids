36426136d817167c3753bcb65a86523a8e382a7e
kyhfan <yh.kim@minivertising.kr>
2014-10-14 20:43:26 +0900

    기본 설정 테스트4

--- include/dbi.php
+++ include/dbi.php
@@ -9,7 +9,8 @@
      *
      ******************************************************************************/
 	
-	$my_db = new mysqli("218.54.31.121", "root", "m!nv#Rtisin9", "tomorrowkids");
+	//$my_db = new mysqli("218.54.31.121", "root", "m!nv#Rtisin9", "tomorrowkids");
+	$my_db = new mysqli("218.54.31.121", "root", "tomorrowkids", "tomorrowkids");
 	if (mysqli_connect_error()) {
 		exit('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
 	}
