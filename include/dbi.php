cb591debf1783f15606146940048734b7127b211
kyhfan <yh.kim@minivertising.kr>
2014-10-14 21:42:06 +0900

    기본 설정 테스트6

--- include/dbi.php
+++ include/dbi.php
@@ -10,7 +10,7 @@
      ******************************************************************************/
 	
 	//$my_db = new mysqli("218.54.31.121", "root", "m!nv#Rtisin9", "tomorrowkids");
-	$my_db = new mysqli("218.54.31.121", "root", "m!nv#Rtisin9", "mysql");
+	$my_db = new mysqli("218.54.31.121", "root", "m!nv#Rtisin9", "tomorrowkids");
 	if (mysqli_connect_error()) {
 		exit('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
 	}
--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 	include_once "config.php";
 
 
-	list($test) = mysql_fetch_array($dbi_tk->execQuery("SELECT count(*) FROM $_gl[tk_member_table]"));
-
 	$query 		= "SELECT count(*) FROM $_gl[tk_member_table]";
 	$result 	= mysqli_query($my_db, $query);
 	$data		= mysqli_fetch_array($result);
