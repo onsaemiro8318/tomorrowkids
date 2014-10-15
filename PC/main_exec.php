5f2e0887410a07bef8faf9f41a64db2d896d6c18
onsaemiro8318 <minivertising@minivertisingui-MacBook-Air.local>
2014-10-15 17:26:42 +0900

    카카오톡 유저 정보 저장

---
+++ PC/main_exec.php
@@ -0,0 +1,27 @@
+<?php
+include_once "../config.php";
+
+switch ($_REQUEST['exec'])
+{
+
+	case "ka_user_info" :
+        $ip_addr = $_SERVER['REMOTE_ADDR'];
+        print_r($_SERVER);
+        $userid	= $_REQUEST['kaUserId'];
+        $media = "kakao";
+        // 유저 정보 체크
+        $query 		= "SELECT * FROM users WHERE user_id = '".$userid."'";
+        $result 	= mysqli_query($my_db, $query);
+    	$data = mysqli_num_rows($result);
+    	if ($data == 0){
+    		$query = "insert into users (user_id, ip_addr, created_at, updated_at, media) values ('".$userid."','".$ip_addr."',now(),now(),'".$media."')";
+    		$result = mysqli_query($my_db, $query);
+            $flag = "Y";
+    	}else {
+            $flag = "N";
+    	}
+        echo $flag;
+    break;
+}
+
+?>
--- config.php
+++ config.php
@@ -1,5 +1,7 @@
 <?
 	session_start();
+    mysqli_query ($my_db,"set names utf8");
+    
 	//환경설정 파일
 	include_once "include/global.php"; 		//변수정보
 	include_once "include/function.php"; 			//함수정보
