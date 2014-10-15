93858317260b557cf71303fabe954c462ede2f42
onsaemiro8318 <minivertising@minivertisingui-MacBook-Air.local>
2014-10-15 18:20:04 +0900

    페이스북 유저 정보 저장

--- js/tk.js
+++ js/tk.js
@@ -1,3 +1,5 @@
+/********************** 카카오톡 **********************/
+
 Kakao.init('5675f40b361955e0b3fcf93944b5d444');
 var jsonStr;
 var obj;
@@ -25,16 +27,14 @@ function kakao_login(){
                  "kaUserId" : obj.id
                }),
                success: function(response){
-                 alert(response);
                  if(response == "Y"){
-                   alert("회원정보가 입력되었습니다.");
                    return;
                  }else{
-                   alert("회원정보가 이미 있습니다.");
                    return;
                  }
                }
              });
+             location.href="work_test.php";
           },
           fail: function(error) {
             alert(JSON.stringify(error))
@@ -47,18 +47,17 @@ function kakao_login(){
     });
 };
 
-
 /********************** 페이스북 **********************/
+      
 var _fbUserId;
 var accessToken;
-var _fbUserName;
 
 function statusChangeCallback(response) {
 	console.log('statusChangeCallback');
 	console.log(response);
+	
 	if (response.status === 'connected') {
 		_fbUserId = response.authResponse.userID;
-    _fbUserName = response.name;
 		accessToken = response.authResponse.accessToken;	
 		// Logged into your app and Facebook.
 	} else if (response.status === 'not_authorized') {
@@ -101,30 +100,40 @@ window.fbAsyncInit = function() {
 
 // Here we run a very simple test of the Graph API after login is
 // successful.  See statusChangeCallback() for when this call is made.
+
+function testAPI() {
+	FB.api('/me', function(response) {
+	});
+}
+
+// 페이스북 로그인
 function facebook_login()
 {
 	FB.login(function(response){
 	_fbUserId = response.authResponse.userID;
-  _fbUserName = response.authResponse.name;
 	accessToken = response.authResponse.accessToken;	
+    $.ajax({
+      type    : "POST",
+      async    : false,
+      //AJAX 처리할것 중복응모체크 (아직 안만들었음)
+      url      : "../PC/main_exec.php",
+      data    : ({
+        "exec" : "fb_user_info" ,
+        "fbUserId" : _fbUserId
+      }),
+      success: function(response){
+        if(response == "Y"){
+          return;
+        }else{
+          return;
+        }
+      }
+    }); 
+    location.href="work_test.php"; 
 	},{scope: 'public_profile,email'});
 }
 
-function facebook_login_check()
-{
-  alert(_fbUserId);
-}
 
-function kakao_login_check()
-{
-  alert(obj.id +" , "+ obj["properties"].nickname +" , "+ ka_access_token +" , "+ ka_refresh_token);
-}
-
-function facebook_logout()
-{
-	FB.logout(function(response) {
-	});
-}
 
 function go_test(num)
 {
