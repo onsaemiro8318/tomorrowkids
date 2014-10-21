d3bea32d259334b8709fe7db33ea40dec57dd60e
kyhfan <yh.kim@minivertising.kr>
2014-10-21 15:40:03 +0900

    쉐어 창 작업2

--- js/tk.js
+++ js/tk.js
@@ -46,8 +46,8 @@ function go_direct_donation()
 
 function show_sns_select_box()
 {
-	alert("<?php echo $_SESSION['ss_media']?>");
-  if( "facebook" == "<?php echo $_SESSION['ss_media']?>"){
+	alert(<?=$_SESSION['ss_media']?>);
+  if( "facebook" == <?php echo $_SESSION['ss_media']?>){
   	$("#sns_select_box_01").show();    
   }else {
   	$("#sns_select_box_02").show();    
