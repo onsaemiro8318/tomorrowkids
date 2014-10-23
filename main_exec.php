d133fc9fdac4b9ad258a1cecc93c9d9e55d70582
kyhfan <yh.kim@minivertising.kr>
2014-10-23 11:07:12 +0900

    테스트 참여 공유, 직접참여 값 수정

--- main_exec.php
+++ main_exec.php
@@ -28,7 +28,8 @@ switch ($_REQUEST['exec'])
 			$_SESSION['ss_mb_idx'] = $mb_idx;
 			$flag = "Y";
 		}else {
-			TK_UpdateUserInfo($userid);
+			$mb_idx = TK_UpdateUserInfo($userid);
+			$_SESSION['ss_mb_idx'] = $mb_idx;
 			$flag = "N";
 		}
 
@@ -49,10 +50,12 @@ switch ($_REQUEST['exec'])
 		$info = TK_GetUserCnt($userid);
 
 		if ($info == 0){
-			TK_InsertUserInfo($userid,$ip_addr,$media,$gubun);
+			$mb_idx = TK_InsertUserInfo($userid,$ip_addr,$media,$gubun);
+			$_SESSION['ss_mb_idx'] = $mb_idx;
 			$flag = "Y";
 		}else {
-			TK_UpdateUserInfo($userid);
+			$mb_idx = TK_UpdateUserInfo($userid);
+			$_SESSION['ss_mb_idx'] = $mb_idx;
 			$flag = "N";
 		}
 
