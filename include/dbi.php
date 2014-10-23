06892107d2f364a2cdc656883745af6fe3ab3514
kyhfan <yh.kim@minivertising.kr>
2014-10-23 13:17:34 +0900

    테스트 참여 공유, 직접참여 값 수정10

--- include/dbi.php
+++ include/dbi.php
@@ -9,7 +9,8 @@
      *
      ******************************************************************************/
 //	$my_db = new mysqli("localhost", "root", "root", "tomorrowkids");	
-	$my_db = new mysqli("localhost", "root", "m!nv#Rtisin9", "tomorrowkids");
+	//$my_db = new mysqli("localhost", "root", "m!nv#Rtisin9", "tomorrowkids");
+	$my_db = mysqli_connect("localhost", "root", "m!nv#Rtisin9", "tomorrowkids");
 	if (mysqli_connect_error()) {
 		exit('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
 	}
