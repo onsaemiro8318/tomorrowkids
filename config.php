17946ad9a7c02869b0931179dbebf478b19c9928
kyhfan <yh.kim@minivertising.kr>
2014-10-27 10:10:46 +0900

    회사 IP 이외의 접속 차단2

--- config.php
+++ config.php
@@ -7,9 +7,6 @@
 	include_once "include/dbi.php"; 			//DB 연결정보
 	include_once "include/page.class.php"; 		//페이지 클래스
 
-	if ($_SERVER['REMOTE_ADDR'] != "61.40.46.36")
-		exit;
-
 	mysqli_query ($my_db,"set names utf8");
 
 	$mobile_agent = array("Iphone","Ipod","Android","Blackberry","SymbianOS|SCH-M\d+","Opera Mini", "Windows ce", "Nokia", "sony" );
@@ -24,6 +21,10 @@
 		$gubun = "MOBILE";
 	else
 		$gubun = "PC";
+
+	if ($_SERVER['REMOTE_ADDR'] != "61.40.46.36" && $gubun == "PC")
+		exit;
+
 
 	$total_count	= TK_GetTestTotalCount();
 
