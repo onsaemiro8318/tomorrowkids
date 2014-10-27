18d0b14adcf354259055b1da1a8a5e17945b7980
kyhfan <yh.kim@minivertising.kr>
2014-10-27 12:24:53 +0900

    테스트 선택시 스타일 변경18

--- PC/ajax_worktest.php
+++ PC/ajax_worktest.php
@@ -12,7 +12,7 @@
 <div>
   <input type="hidden" name="sel_value" id="sel_value">
   <input type="hidden" name="selected_value" id="selected_value" value="<?=$_POST[selected_val]?>">
-  <input type="text" name="selected_answer" id="selected_answer" value="">
+  <input type="hidden" name="selected_answer" id="selected_answer" value="">
   <h1><?=$question_data[test_value]?></h1>
   <p id="answer1" onmouseover="select_answer(this.id,'over');" onmouseout="select_answer(this.id,'out')" onclick="select_answer(this.id,'click');"><a href="javascript:save_info('<?=$answer_data[0][idx]?>');"><?=$answer_data[0][test_value]?></a></p>
   <p id="answer2" onmouseover="select_answer(this.id,'over');" onmouseout="select_answer(this.id,'out')" onclick="select_answer(this.id,'click');"><a href="javascript:save_info('<?=$answer_data[1][idx]?>');"><?=$answer_data[1][test_value]?></a></p>
