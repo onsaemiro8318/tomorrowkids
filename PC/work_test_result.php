932875d193f9ec33ecd20382b4b4aedc53b2dbfd
onsaemiro8318 <minivertising@minivertisingui-MacBook-Air.local>
2014-10-28 20:36:27 +0900

    PC 결과화면

--- PC/work_test_result.php
+++ PC/work_test_result.php
@@ -9,89 +9,75 @@
 	$test_idx	= $_REQUEST[idx];
 
 ?>
+?>
 <script>
     window.history.forward(0);
 </script>
-  <body>
-    <div class="backLayer" style="display:none;background-color:black;position:absolute;left:0px;top:0px;z-index:999;"></div>
-    <div style="position:absolute;">
-      <div style="position:top;width:100%;height:60px;background:green">
-        <a href="index.php">내일을 부탁해</a>
-        <p><?=$total_count?>명의 내일(work)이 아이들의 내일(tomorrow)이 되고 있습니다.</p>
-      </div>
-      <div style="position:top;width:100%;height:110px;background:skyblue;">
-        <h1>STEP2. 내일(work) 공유로 기부하기</h1>
-        <p>당신의 내일(work) 테스트 결과 입니다.</p>
-      </div>
-      <div style="position:relative;width:100%;height:300px;background:orange;" id="test_result_div">
-<?=$user_job[job];?>
-<br />
-------------------------------------------------------------------------------
-<br />
-<?=$user_job[job_explain];?>
-      </div>
-      <div style="position:top;width:100%;height:200px;background:skyblue;">
-        <p>세상에는 이처럼 당신이 하고 있는 일, 상상치도 못했던 다양한 내 일(work)이 많이 있답니다.
-           빈곤한 환경으로 이런 일이 있다는 것 조차 모른 채 제한된 꿈을 꾸는 아이들을 도와주세요.
-           당신의 내일(work) 결과를 SNS에 공유하시면, 아이들의 내일(Tomorrow)을 위한 기부로 이어집니다.
-        </p>
+<body>
+<div class="main_wrapper">
+<!--Line1 start-->
+<div class="main_top1">
+	<div class="subtopinbg1">
+    	<h1><a href=""><img src="images/logo.png" alt="dreamfull"/></a></h1>
+        <div class="fl_right sub_toprite">
+        	<span class="toptext">1,000명의 내일이 모이면<br/>아이들의 내일을 위한<br/>특별 강연회가 열립니다!</span>
+            <div class="number_sub">
+            	<ul>
+            	<li>1</li>
+                <li class="number2">0</li>
+                <li class="number3">0</li>
+                <li class="number4">0</li>
+            </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!--Line1 end-->
+<!--Line2 start-->
+<div class="sub_top2">
+	<div class="sub_top2in">
+    	<p class="bluetext">STEP2</p>
+        <p class="whitetext">내일 (Work) 공유로 기부하기</p>
+        <div class="result_box"><div class="bluebox_result fl_left"><img src="images/result_img1.jpg"/></div>
+        	 <div class="fl_left">
+        		<div class="re_text">당신의 내일(Work) 테스트 결과입니다.</div>
+                <div class="resulttwotext">
+                	<p class="big">"<?=$user_job[job];?>"</p>
+                    <p class="sm"><?=$user_job[job_explain];?></p>
+                </div>
+      		 </div>
+        </div>
+        <div class="undertext">세상에는 이처럼 당신이 하고 있는 일, 상상치도 못했던 다양한 내 일(Work) 이 많이 있답니다.<br/>
+빈곤한 환경으로 이런 일이 있다는 것 조차 모른 채 제한된 꿈을 꾸는 아이들을 도와주세요.<br/>
+당신의 내일(Work) 결과를 SNS에 공유하시면, 아이들의 내일(Tomorrow)을 위한 기부로 이어집니다.</div>
 <?
 	if ($user_info[media] == $_gl[login_media]['facebook'])
 	{
 ?>
-        <a href="#" onclick="fb_share('<?=$user_job[job]?>','<?=$user_job[job_explain]?>','<?=$test_idx?>');">페이스북 공유</a>
+	<div class="facebookbut"><a href="#" onclick="fb_share('<?=$user_job[job]?>','<?=$user_job[job_explain]?>','<?=$test_idx?>');"><img src="images/facebook_sub.png"/></a></div>
 <?
 	}else{
 ?>
-        <a href="#" onclick="ks_share('<?=$user_job[job]?>','<?=$user_job[job_explain]?>','<?=$test_idx?>');">카카오스토리 공유</a>
+	<div class="kakaostorybut"><a href="#" onclick="ks_share('<?=$user_job[job]?>','<?=$user_job[job_explain]?>','<?=$test_idx?>');"><img src="images/facebook_sub.png"/></a></div>
 <?
 	}
 ?>
-        <!-- <a href="#" onclick="show_sns_select_box('<?=$_SESSION['ss_media']?>');">공유로 기부하기</a>
-        <a href="http://www.dreamfull.or.kr/app/newdf/main" onclick="go_direct_donation();" target="_blank">직접 후원하고 싶다면?</a> -->
-      </div>
-    <div id="email_div" class="popup_div" style="display:none;position:absolute;width:50%;height:30%;top:30%;margin-left:5%;background:gray;z-index:1000;">
-      감사합니다. 공유로 기부가 완료 되셨습니다.<br />
-      캠페인 결과와 강연회 소식을 이메일로 알려드립니다.<br />
-      <input type="text" name="email1" id="email1"> @ 
-      <input type="text" name="email2" id="email2">
-      <select name="sel_email" onchange="input_email(this.value)">
-        <option value="">직접입력</option>
-        <option value="gmail.com">gmail.com</option>
-        <option value="hanmail.net">hanmail.net</option>
-        <option value="hitel.net">hitel.net</option>
-        <option value="hotmail.com">hotmail.com</option>
-        <option value="korea.com">korea.com</option>
-        <option value="nate.com">nate.com</option>
-        <option value="naver.com">naver.com</option>
-        <option value="outlook.com">outlook.com</option>
-      </select>
-      <input type="button" value="확인" onclick="update_user_email('<?=$test_idx?>');">
     </div>
-    <div id="donation_div" class="popup_div" style="display:none;position:absolute;width:50%;height:30%;top:30%;margin-left:5%;background:gray;z-index:1000;">
-        아이들을 2배 더 후원하고 싶은 분은<br />
-        아래 직접 기부하기에도 참여해보세요!<br />
-        <a href="#" onclick="go_direct_donation('<?=$test_idx?>');">직접 기부하기</a>
+</div>
+<!--Line2 end-->
+<!--Line3 start-->
+<div class="main_top3">
+<!--Footer start-->
+<div class="footer">
+	<div class="lineone"><div class="img"><img src="images/footer_line1_text.png"/></div></div>
+    <div class="linetwo">
+    	<div class="inbox"><span class="fl_left"><img src="images/footer_line2_text.png"/></span><span class="fl_right"><img src="images/footer_line2_logo.png"/></span></div>
     </div>
-    </div>
-  </body>
+</div>
+<!--Footer end-->
+</div>
+<!--Line3 end-->
+</div>
+<div class="mask"></div>
+</body>
 </html>
-<script type="text/javascript">
-	$(window).resize(function(){
-        var width = $(window).width();
-        var height = $(window).height();
-        $(".backLayer").width(width).height(height);
-    });
-    $(document).keydown(function(event){
-        if(event.which=='27'){
-        $(".popup_div").fadeOut(500);
-        $(".backLayer").fadeOut(500);
-        }
-    });
-    $(document).ready(function(){
-    $(".backLayer").click(function(){
-        $(".backLayer").fadeOut(500);
-        $(".popup_div").fadeOut(500);
-    });
-    });
-</script>
