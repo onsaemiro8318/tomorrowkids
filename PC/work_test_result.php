ba646961ed5a8c632f9a2f924f9026b179d639a8
kyhfan <yh.kim@minivertising.kr>
2014-10-29 17:32:37 +0900

    PC 개인정보 팝업 작업11

--- PC/work_test_result.php
+++ PC/work_test_result.php
@@ -129,7 +129,7 @@
   <div class="donationbt"><a href="#" onclick="go_direct_donation('<?=$test_idx?>');"><img src="images/donation_pbut.jpg"/></a></div>
 </div>
 
-<div class="popupbg3" id="privacy_term" style="display:block">
+<div class="popupbg3" id="privacy_term">
   <div class="closeic"><a href="javascript:close_popup3()"><img src="images/close_icon.gif"/></a></div>
   <div class="donationbt" style="width:400px;padding-top:80px">
     <p style="margin-bottom:10px"><font style="font-size:20px"><b>사단법인 부스러기사랑나눔회 귀하</b></font></p>
