b215df248cb76046d5e175f28895b65b4a17a46e
kyhfan <yh.kim@minivertising.kr>
2014-10-29 10:26:47 +0900

    PC테스트 페이지 수정8

--- PC/work_test.php
+++ PC/work_test.php
@@ -70,7 +70,7 @@
             <div class="fl_left textinbox">
               <p class="bluetext"><?=$question_data[test_value]?></p>
               <p>
-			    <div class="fl_left tag">A.</div><span class="anstext fl_left" id="answer1" onmouseover="select_answer(this.id,'over');" onmouseout="select_answer(this.id,'out')" onclick="select_answer(this.id,'click');save_info('<?=$answer_data[0][idx]?>')" style="cursor:pointer;"><?=$answer_data[0][test_value]?></span></p>
+			    <div class="fl_left tag">A.</div><span class="anstextone fl_left" id="answer1" onmouseover="select_answer(this.id,'over');" onmouseout="select_answer(this.id,'out')" onclick="select_answer(this.id,'click');save_info('<?=$answer_data[0][idx]?>')" style="cursor:pointer;padding-bottom:20px"><?=$answer_data[0][test_value]?></span></p>
               <p><div class="fl_left tag">B.</div><span class="anstextone fl_left" id="answer2" onmouseover="select_answer(this.id,'over');" onmouseout="select_answer(this.id,'out')" onclick="select_answer(this.id,'click');save_info('<?=$answer_data[0][idx]?>')" style="cursor:pointer;"><?=$answer_data[1][test_value]?></span></p>
             </div>
           </div>
