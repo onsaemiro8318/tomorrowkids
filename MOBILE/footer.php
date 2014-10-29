9368acdde99961c43b05e19dcd912636cd7a458f
onsaemiro8318 <minivertising@minivertisingui-MacBook-Air.local>
2014-10-29 15:40:38 +0900

    mobile test result page 기능 구현

--- MOBILE/footer.php
+++ MOBILE/footer.php
@@ -4,11 +4,14 @@
         <span class="hankologo">기부후원으로 진행됩니다.</span>
       </div>
       <div class="line2">
-        <div class="logoline"><img src="images/line2_logo.png"/></div>
-        <div class="textline2"><a href="http://www.busrugy.or.kr/USR_main.asp??=Aboutus/index" target="_blank">부스러기사랑나눔회 소개</a> | <a href="http://www.dreamfull.or.kr/app/newdf/popup/agreement" target="_blank">이용약관</a> | <a href="http://www.dreamfull.or.kr/app/newdf/popup/personal_policy" target="_blank">개인정보처리방침</a> | <a href="http://www.dreamfull.or.kr/app/newdf/popup/donation_policy" target="_blank">기부정책</a> | <br/>
-본 사이트는 [㈜엠프론티어]의 재능기부로 제작되었습니다.<br/>
-사단법인 부스러기사랑나눔회 / 대표자 : 강명순 / 사업자번호 : 110-82-08381 <br/>
-서울시 용산구 청파로 46 한통빌딩 10층 / 문의 : 070-7164-7215/7209<br/>
+    	<div class="logoline hidden">
+        	<span class="link1"><a href=""><img src="images/line2_logo1.png"/></a></span>
+              <span class="link2"><a href=""><img src="images/line2_logo2.png"/></a></span>
+        </div>
+        <div class="textline2"><a href="http://www.busrugy.or.kr/USR_main.asp??=Aboutus/index" target="_blank">부스러기사랑나눔회 소개</a> | <a href="http://www.dreamfull.or.kr/app/newdf/popup/agreement" target="_blank">이용약관</a> | <a href="http://www.dreamfull.or.kr/app/newdf/popup/personal_policy" target="_blank">개인정보처리방침</a> | <a href="http://www.dreamfull.or.kr/app/newdf/popup/donation_policy" target="_blank">기부정책</a> | 
+본 사이트는 [㈜엠프론티어]의 재능기부로 제작되었습니다.
+사단법인 부스러기사랑나눔회 / 대표자 : 강명순 / 사업자번호 : 110-82-08381 
+서울시 용산구 청파로 46 한통빌딩 10층 / 문의 : 070-7164-7215/7209 
 / 카카오톡 ID : @드림풀
         </div>
       </div>
