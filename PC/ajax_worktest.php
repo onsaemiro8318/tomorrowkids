f9977988ba4b938aa665a657591d70662dd2a5e0
kyhfan <yh.kim@minivertising.kr>
2014-10-29 10:53:06 +0900

    PC테스트 페이지 수정12

--- PC/ajax_worktest.php
+++ PC/ajax_worktest.php
@@ -9,37 +9,36 @@
 
 	$next_num		= $_POST[test_idx] + 1;
 ?>
-<div>
   <input type="hidden" name="sel_value" id="sel_value">
   <input type="hidden" name="selected_value" id="selected_value" value="<?=$_POST[selected_val]?>">
   <input type="hidden" name="selected_answer" id="selected_answer" value="">
-  <h1><?=$question_data[test_value]?></h1>
-  <p id="answer1" onmouseover="select_answer(this.id,'over');" onmouseout="select_answer(this.id,'out')" onclick="select_answer(this.id,'click');"><a href="javascript:save_info('<?=$answer_data[0][idx]?>');"><?=$answer_data[0][test_value]?></a></p>
-  <p id="answer2" onmouseover="select_answer(this.id,'over');" onmouseout="select_answer(this.id,'out')" onclick="select_answer(this.id,'click');"><a href="javascript:save_info('<?=$answer_data[1][idx]?>');"><?=$answer_data[1][test_value]?></a></p>
-  <a href="javascript:go_next_question('<?=$next_num?>','<?=$_POST[selected_val]?>');">답변 선택</a>
-</div>
+  <div class="hidden">
+    <div class="fl_left hidden plicon"><img src="images/step1_icon.jpg"/></div>
+    <div class="fl_left textinbox">
+      <p class="bluetext"><?=$question_data[test_value]?></p>
+      <p>
+        <div class="fl_left tag">A.</div><span class="anstextone fl_left" id="answer1" onmouseover="select_answer(this.id,'over');" onmouseout="select_answer(this.id,'out')" onclick="select_answer(this.id,'click');save_info('<?=$answer_data[0][idx]?>')" style="cursor:pointer;padding-bottom:20px"><?=$answer_data[0][test_value]?></span></p>
+      <p><div class="fl_left tag">B.</div><span class="anstextone fl_left" id="answer2" onmouseover="select_answer(this.id,'over');" onmouseout="select_answer(this.id,'out')" onclick="select_answer(this.id,'click');save_info('<?=$answer_data[0][idx]?>')" style="cursor:pointer;"><?=$answer_data[1][test_value]?></span></p>
+    </div>
+  </div>
+  <div class="hidden nextbut"><a href="javascript:go_next_question('<?=$next_num?>','<?=$_POST[selected_val]?>');"><img src="images/next_but.jpg" alt="다음 질문"/></a></div>
+
 <script type="text/javascript">
-	$(document).ready(function(){
-		if ($("#selected_answer").val() != "")
-			$("#"+ sel_answer).css("font-weight","bold");
-	});
-
-
 	function select_answer(answer_id, event_id)
 	{
 		var sel_answer = $("#selected_answer").val();
 		if (event_id == "over"){
-			$("#"+answer_id).css("font-weight","bold");
+			$("#"+answer_id).attr('class','anstext fl_left');
 		}else if (event_id == "click"){
-			$("#"+answer_id).css("font-weight","bold");
+			$("#"+answer_id).attr('class','anstext fl_left');
 			$("#selected_answer").val(answer_id);
 			if (answer_id == "answer1")
-				$("#answer2").css("font-weight","normal");
+				$("#answer2").attr('class','anstextone fl_left');
 			else
-				$("#answer1").css("font-weight","normal");
+				$("#answer1").attr('class','anstextone fl_left');
 		}else{
 			if (sel_answer != answer_id){
-				$("#"+answer_id).css("font-weight","normal");
+				$("#"+answer_id).attr('class','anstextone fl_left');
 			}
 		}
 	}
