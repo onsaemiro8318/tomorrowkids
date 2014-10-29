adeaa9ec0e5d3382f8d69d306c44d1c8b8dde584
kyhfan <yh.kim@minivertising.kr>
2014-10-29 11:53:36 +0900

    모바일 viewport 제거

--- MOBILE/header.php
+++ MOBILE/header.php
@@ -3,7 +3,7 @@
     <title>TomorrowKids - 내일을 부탁해</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-	<meta name="viewport" content="width=device-width, user-scalable=no">
+	<!-- <meta name="viewport" content="width=device-width, user-scalable=no"> -->
     <script src="https://developers.kakao.com/sdk/js/kakao.min.js"></script>
     <script type='text/javascript' src='../js/jquery-1.11.0.min.js'></script>
     <script type='text/javascript' src='../js/tk.js'></script>
