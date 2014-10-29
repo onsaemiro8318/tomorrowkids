57f3619b482c54e4a9ebf8b9dff93324dc67cab8
kyhfan <yh.kim@minivertising.kr>
2014-10-29 10:15:00 +0900

    PC테스트 페이지 수정5

--- PC/work_test.php
+++ PC/work_test.php
@@ -69,8 +69,13 @@
             <div class="fl_left hidden plicon"><img src="images/step1_icon.jpg"/></div>
             <div class="fl_left textinbox">
               <p class="bluetext"><?=$question_data[test_value]?></p>
-              <p id="answer1" onmouseover="select_answer(this.id,'over');" onmouseout="select_answer(this.id,'out')" onclick="select_answer(this.id,'click');save_info('<?=$answer_data[0][idx]?>')" style="cursor:pointer;"><div class="fl_left tag">A.</div><span class="anstext fl_left"><?=$answer_data[0][test_value]?></span></p>
-              <p id="answer2" onmouseover="select_answer(this.id,'over');" onmouseout="select_answer(this.id,'out')" onclick="select_answer(this.id,'click');save_info('<?=$answer_data[0][idx]?>')" style="cursor:pointer;"><div class="fl_left tag">B.</div><span class="anstextone fl_left"><?=$answer_data[1][test_value]?></span></p>
+              <p id="answer1" onmouseover="select_answer(this.id,'over');" onmouseout="select_answer(this.id,'out')" onclick="save_info('<?=$answer_data[0][idx]?>')" style="cursor:pointer;">
+                <div class="fl_left tag">A.</div>
+                <span class="anstext fl_left"><?=$answer_data[0][test_value]?></span>
+              </p>
+              <p id="answer2" onmouseover="select_answer(this.id,'over');" onmouseout="select_answer(this.id,'out')" onclick="select_answer(this.id,'click');save_info('<?=$answer_data[0][idx]?>')" style="cursor:pointer;">
+                <div class="fl_left tag">B.</div><span class="anstextone fl_left"><?=$answer_data[1][test_value]?></span>
+              </p>
             </div>
           </div>
           <div class="hidden nextbut"><a href="javascript:go_next_question('<?=$next_num?>','');""><img src="images/next_but.jpg" alt="다음 질문"/></a></div>
@@ -104,6 +109,8 @@
 
 	function select_answer(answer_id, event_id)
 	{
+		alert('111');
+		/*
 		var sel_answer = $("#selected_answer").val();
 		if (event_id == "over"){
 			$("#"+answer_id).attr('class','anstext fl_left');
@@ -119,6 +126,7 @@
 				$("#"+answer_id).attr('class','anstextone fl_left');
 			}
 		}
+		*/
 	}
 
 </script>
