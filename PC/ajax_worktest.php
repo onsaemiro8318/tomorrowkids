011991857b62aaea61697e441d8ab97e5b570807
kyhfan <yh.kim@minivertising.kr>
2014-10-31 22:41:41 +0900

    카스 공유 테스트5

--- PC/ajax_worktest.php
+++ PC/ajax_worktest.php
@@ -3,32 +3,32 @@
 	include_once "../config.php";
 
 	// 질문 정보
-	$question_data	= TK_GetTestQuestionInfo($_POST[test_idx]);
+	$question_data	= TK_GetTestQuestionInfo($_POST['test_idx']);
 
-	$answer_data	= TK_GetTestAnswerInfo($_POST[test_idx]);
+	$answer_data	= TK_GetTestAnswerInfo($_POST['test_idx']);
 
-	$next_num		= $_POST[test_idx] + 1;
+	$next_num		= $_POST['test_idx'] + 1;
 ?>
   <input type="hidden" name="sel_value" id="sel_value">
-  <input type="hidden" name="selected_value" id="selected_value" value="<?=$_POST[selected_val]?>">
+  <input type="hidden" name="selected_value" id="selected_value" value="<?=$_POST['selected_val']?>">
   <input type="hidden" name="selected_answer" id="selected_answer" value="">
   <div class="hidden">
     <div class="fl_left hidden plicon"><img src="images/step1_icon.jpg"/></div>
     <div class="fl_left textinbox">
-      <p class="bluetext"><?=$question_data[test_value]?></p>
+      <p class="bluetext"><?=$question_data['test_value']?></p>
       <p>
-        <div class="fl_left tag">A.</div><span class="anstextone fl_left" id="answer1" onmouseover="select_answer(this.id,'over');" onmouseout="select_answer(this.id,'out')" onclick="select_answer(this.id,'click');save_info('<?=$answer_data[0][idx]?>')" style="cursor:pointer;padding-bottom:20px"><?=$answer_data[0][test_value]?></span></p>
-      <p><div class="fl_left tag">B.</div><span class="anstextone fl_left" id="answer2" onmouseover="select_answer(this.id,'over');" onmouseout="select_answer(this.id,'out')" onclick="select_answer(this.id,'click');save_info('<?=$answer_data[1][idx]?>')" style="cursor:pointer;"><?=$answer_data[1][test_value]?></span></p>
+        <div class="fl_left tag">A.</div><span class="anstextone fl_left" id="answer1" onmouseover="select_answer(this.id,'over');" onmouseout="select_answer(this.id,'out')" onclick="select_answer(this.id,'click');save_info('<?=$answer_data[0]['idx']?>')" style="cursor:pointer;padding-bottom:20px"><?=$answer_data[0]['test_value']?></span></p>
+      <p><div class="fl_left tag">B.</div><span class="anstextone fl_left" id="answer2" onmouseover="select_answer(this.id,'over');" onmouseout="select_answer(this.id,'out')" onclick="select_answer(this.id,'click');save_info('<?=$answer_data[1]['idx']?>')" style="cursor:pointer;"><?=$answer_data[1]['test_value']?></span></p>
     </div>
   </div>
 <?
     if($next_num == 11){
 ?>
-  <div class="hidden nextbut"><a href="javascript:go_next_question('<?=$next_num?>','<?=$_POST[selected_val]?>');"><img src="images/go_result_bt_pc.png" alt="결과 보기"/></a></div>
+  <div class="hidden nextbut"><a href="javascript:go_next_question('<?=$next_num?>','<?=$_POST['selected_val']?>');"><img src="images/go_result_bt_pc.png" alt="결과 보기"/></a></div>
 <?
     }else{
 ?>
-  <div class="hidden nextbut"><a href="javascript:go_next_question('<?=$next_num?>','<?=$_POST[selected_val]?>');"><img src="images/next_but.jpg" alt="다음 질문"/></a></div>
+  <div class="hidden nextbut"><a href="javascript:go_next_question('<?=$next_num?>','<?=$_POST['selected_val']?>');"><img src="images/next_but.jpg" alt="다음 질문"/></a></div>
 <?
     }
 ?>
