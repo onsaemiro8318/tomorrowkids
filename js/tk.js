821f278a992bba6381dc88be5dfa81d8fac27a2f
onsaemiro8318 <minivertising@minivertisingui-MacBook-Air.local>
2014-10-31 17:54:39 +0900

    fb chrome iOS 오류 체크

--- js/tk.js
+++ js/tk.js
@@ -474,6 +474,9 @@ function testAPI() {
 // 페이스북 로그인
 function facebook_login()
 {
+  if( navigator.userAgent.match('CriOS') ){
+    window.open('https://www.facebook.com/dialog/oauth?client_id=293604627507652&redirect_uri=http://www.tomorrowkids.or.kr/MOBILE/work_test.php&scope=public_profile,email', '', null);
+  }else{
 	FB.login(function(response){
 		_fbUserId = response.authResponse.userID;
 		accessToken = response.authResponse.accessToken;
@@ -516,6 +519,7 @@ function facebook_login()
 		}); 
 		//location.href="work_test.php"; 
 	},{scope: 'public_profile,email'});
+  }
 }
 
 function facebook_logout()
