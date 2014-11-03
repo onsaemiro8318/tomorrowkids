d93ea615431efe6132d69a9c240b2a6c50e502f3
kyhfan <yh.kim@minivertising.kr>
2014-11-03 13:30:31 +0900

    질문 선택시 다음질문 자동 넘어가기9

--- PC/ajax_worktest.php
+++ PC/ajax_worktest.php
@@ -18,7 +18,7 @@
       <p class="bluetext"><?=$question_data['test_value']?></p>
       <p>
         <div class="fl_left tag">A.</div><span class="anstextone fl_left" id="answer1" onmouseover="select_answer(this.id,'over');" onmouseout="select_answer(this.id,'out')" onclick="go_next_question('<?=$answer_data[0]['idx']?>','<?=$next_num?>','<?=$_POST['selected_val']?>')" style="cursor:pointer;padding-bottom:20px"><?=$answer_data[0]['test_value']?></span></p>
-      <p><div class="fl_left tag">B.</div><span class="anstextone fl_left" id="answer2" onmouseover="select_answer(this.id,'over');" onmouseout="select_answer(this.id,'out')" onclick="go_next_question('<?=$answer_data[0]['idx']?>','<?=$next_num?>','<?=$_POST['selected_val']?>')" style="cursor:pointer;"><?=$answer_data[1]['test_value']?></span></p>
+      <p><div class="fl_left tag">B.</div><span class="anstextone fl_left" id="answer2" onmouseover="select_answer(this.id,'over');" onmouseout="select_answer(this.id,'out')" onclick="go_next_question('<?=$answer_data[1]['idx']?>','<?=$next_num?>','<?=$_POST['selected_val']?>')" style="cursor:pointer;"><?=$answer_data[1]['test_value']?></span></p>
     </div>
   </div>
 <?
