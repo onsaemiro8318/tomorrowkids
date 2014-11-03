85aa37946de46d3b0e4ab74bda0bad1c9672d5ad
kyhfan <yh.kim@minivertising.kr>
2014-11-03 13:48:32 +0900

    질문 선택시 다음질문 자동 넘어가기13

--- MOBILE/work_test.php
+++ MOBILE/work_test.php
@@ -19,7 +19,7 @@
 	$question_data	= TK_GetTestQuestionInfo($_POST['test_idx']);
 
 	$answer_data	= TK_GetTestAnswerInfo($_POST['test_idx']);
-
+print_r($answer_data);
 	$next_num		= $_POST['test_idx'] + 1;
 
 
@@ -60,11 +60,11 @@
       <div class="quas">
         <?=$question_data['test_value']?>
       </div>
-      <div class="ansbox" onclick="go_next_question('<?=$answer_data[0]['idx']?>','<?=$next_num?>','')" style="cursor:pointer;">
+      <div class="ansbox">
         <div class="fl_left tag">A.</div>
         <div class="fl_left tagtext2" id="answer1" onmouseover="select_answer(this.id,'over');" onmouseout="select_answer(this.id,'out')" onclick="go_next_question('<?=$answer_data[0]['idx']?>','<?=$next_num?>','')"><?=$answer_data[0]['test_value']?></div>
       </div>
-      <div class="ansbox" onclick="go_next_question('<?=$answer_data[1]['idx']?>','<?=$next_num?>','')" style="cursor:pointer;">
+      <div class="ansbox">
         <div class="fl_left tag">B.</div>
         <div class="fl_left tagtext2" id="answer2" onmouseover="select_answer(this.id,'over');" onmouseout="select_answer(this.id,'out')" onclick="go_next_question('<?=$answer_data[1]['idx']?>','<?=$next_num?>','')"><?=$answer_data[1]['test_value']?></div>
       </div>
