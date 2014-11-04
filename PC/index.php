72210897e9fb5d1127fdc614d828761ac5365031
kyhfan <yh.kim@minivertising.kr>
2014-11-04 17:25:26 +0900

    줄맞춤 수정

--- PC/index.php
+++ PC/index.php
@@ -38,8 +38,8 @@
 	}, 3000)
 
 	$(window).resize(function(){
-        var width = $(window).width();
-        //var height = $(window).height();
+		var width = $(window).width();
+		//var height = $(window).height();
 		var height = 0;
 
 		if( browser.msie ){ //IE
@@ -58,14 +58,14 @@
 		}
 
 		$(".mask").width(width).height(height);
-    });
-    $(document).keydown(function(event){
-        if(event.which=='27'){
+	});
+	$(document).keydown(function(event){
+		if(event.which=='27'){
 			$(".video_fremebox").fadeOut(500);
 			$(".mask").fadeOut(500);
 		//	$(".video_but").show();
-        }
-    });
+		}
+	});
 	$(document).ready(function(){
 		$(".mask").click(function(){
 			$(".mask").fadeOut(500);
@@ -81,65 +81,65 @@
     </script>
     
 <body>
-<!--Line1 start-->
-<div class="main_wrapper">
-<div class="main_top1">
-	<div class="main_topinbg1">
-    	<h1><a href="http://www.dreamfull.or.kr" target="_blank"><img src="images/logo.png" alt="dreamfull"/></a></h1>
+  <!--Line1 start-->
+  <div class="main_wrapper">
+    <div class="main_top1">
+      <div class="main_topinbg1">
+        <h1><a href="http://www.dreamfull.or.kr" target="_blank"><img src="images/logo.png" alt="dreamfull"/></a></h1>
         <div class="videobox"><div class="video_but"><a href="#" onclick="play_movie('<?=$gubun?>');"><img src="images/play_but.png"/></a></div></div>
+      </div>
     </div>
-</div>
-<!--Line1 end-->
-<!--Line2 start-->
-<div class="main_top2">
-	<div class="main_topin2">
-    <p class="text_data"><span class="f_text">10,000명의 내일이 모이면</span><br/><span class="t_text">아이들의 내일을 위한 특별 강연회</span>가 열립니다!</p>
-    <div class="gr_box">
-    	<div class="greenco_left fl_left"></div><div class="greenco_right fl_left"><div class="peopleic"><img src="images/gr_peopleicon.png"/></div></div><div class="minic"><img src="images/gr_15min.png"/></div><div class="line_number"><img src="images/gr_number_line.png"/></div>
-    </div>
-    <div class="number_boxwr">
-    	<div class="fl_left peoplenumber">현재 참여자수</div>
-        <div class="number_box fl_left">
-        	<ul>
-            	<li><?=$t_count1?></li>
-            	<li class="number2"><?=$t_count2?></li>
-                <li class="number3"><?=$t_count3?></li>
-                <li class="number4"><?=$t_count4?></li>
-                <li class="number5"><?=$t_count5?></li>
-            </ul>
+  <!--Line1 end-->
+  <!--Line2 start-->
+    <div class="main_top2">
+      <div class="main_topin2">
+        <p class="text_data"><span class="f_text">10,000명의 내일이 모이면</span><br/><span class="t_text">아이들의 내일을 위한 특별 강연회</span>가 열립니다!</p>
+        <div class="gr_box">
+          <div class="greenco_left fl_left"></div><div class="greenco_right fl_left"><div class="peopleic"><img src="images/gr_peopleicon.png"/></div></div><div class="minic"><img src="images/gr_15min.png"/></div><div class="line_number"><img src="images/gr_number_line.png"/></div>
         </div>
+        <div class="number_boxwr">
+          <div class="fl_left peoplenumber">현재 참여자수</div>
+          <div class="number_box fl_left">
+            <ul>
+              <li><?=$t_count1?></li>
+              <li class="number2"><?=$t_count2?></li>
+              <li class="number3"><?=$t_count3?></li>
+              <li class="number4"><?=$t_count4?></li>
+              <li class="number5"><?=$t_count5?></li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
-    </div>
-</div>
 <!--Line2 end-->
 <!--Line3 start-->
-<div class="main_top3">
-	<div class="soc_linkbox">
-    	<div class="hidden"><span class="fl_left facebookic"><a href="#" onclick="facebook_login();"><img src="images/facebook_but.png"/></a></span><span class="fl_left"><a href="#" onclick="kakao_login();"><img src="images/kakaotalk_but.png"/></a></span></div>
+    <div class="main_top3">
+      <div class="soc_linkbox">
+        <div class="hidden"><span class="fl_left facebookic"><a href="#" onclick="facebook_login();"><img src="images/facebook_but.png"/></a></span><span class="fl_left"><a href="#" onclick="kakao_login();"><img src="images/kakaotalk_but.png"/></a></span></div>
         <p class="text hidden">* 어떠한 정보도 무단으로 포스팅하지 않습니다.</p>
-    </div>
-    <div class="stepbox hidden">
-    	<div><img src="images/step_fiveicon.png"/></div>
-        	<ul>
-            	<li>내일<br/>테스트하기</li>
-                <li class="stept1">내일 결과<br/>SNS에 공유하기</li>
-                <li class="stept2">참여를 통한<br/>기부 완료</li>
-                <li class="stept3">세상을 바꾸는 시간,15분 과<br/>함께 하는 아이들 교육<br/>프로그램 제작</li>
-            </ul>
-    </div>
-    <div class="donation"><img src="images/donation_img.png"/><div class="donationbutton"><span class="fl_left pinkbut"><a href="http://www.dreamfull.or.kr/app/newdf/donation/collection_box_detail?themeNo=201401070002" target="_blank"><img src="images/donation_but_pink.png"/></a></span><span class="fl_left"><a href="http://www.dreamfull.or.kr/app/newdf/donation/collection_box" target="_blank"><img src="images/donation_but_green.png"/></a></span></div>
+      </div>
+      <div class="stepbox hidden">
+        <div><img src="images/step_fiveicon.png"/></div>
+        <ul>
+          <li>내일<br/>테스트하기</li>
+          <li class="stept1">내일 결과<br/>SNS에 공유하기</li>
+          <li class="stept2">참여를 통한<br/>기부 완료</li>
+          <li class="stept3">세상을 바꾸는 시간,15분 과<br/>함께 하는 아이들 교육<br/>프로그램 제작</li>
+        </ul>
+      </div>
+      <div class="donation"><img src="images/donation_img.png"/><div class="donationbutton"><span class="fl_left pinkbut"><a href="http://www.dreamfull.or.kr/app/newdf/donation/collection_box_detail?themeNo=201401070002" target="_blank"><img src="images/donation_but_pink.png"/></a></span><span class="fl_left"><a href="http://www.dreamfull.or.kr/app/newdf/donation/collection_box" target="_blank"><img src="images/donation_but_green.png"/></a></span></div>
     </div>
 <!--Footer start-->
 <?
 	include_once "footer.php";
 ?>
 <!--Footer end-->
-</div>
+  </div>
 <!--Line3 end-->
 </div>
 <div class="mask"></div>
 <div class="video_fremebox">
-	<div class="video"><iframe id="ytplayer" width="951px" height="579px" src="<?=$_gl['youtube_url']?>" frameborder="0" allowfullscreen></iframe></div>
+  <div class="video"><iframe id="ytplayer" width="951px" height="579px" src="<?=$_gl['youtube_url']?>" frameborder="0" allowfullscreen></iframe></div>
 </div>
 </body>
 </html>
--- PC/work_test_result.php
+++ PC/work_test_result.php
@@ -64,30 +64,30 @@
 <!--Line1 end-->
 <!--Line2 start-->
 <div class="sub_top2">
-	<div class="sub_top2in">
-    	<p class="bluetext">STEP2</p>
-        <p class="whitetext">내일 (Work) 공유로 기부하기</p>
-        <div class="result_box"><div class="bluebox_result fl_left"><img src="<?=$job_imgurl?>"/></div>
-        	 <div class="fl_left">
-        		<div class="re_text">당신의 내일(Work) 테스트 결과입니다.</div>
-                <div class="resulttwotext" style="width:460px;height:230px">
-                	<p class="big">"<?=$user_job['job'];?>"</p>
-                    <p class="sm"><?=$user_job['job_explain'];?></p>
-                </div>
-      		 </div>
-        </div>
-        <div class="undertext">세상에는 이처럼 당신이 하고 있는 일, 상상치도 못했던 다양한 내 일(Work) 이 많이 있답니다.<br/>
+  <div class="sub_top2in">
+    <p class="bluetext">STEP2</p>
+    <p class="whitetext">내일 (Work) 공유로 기부하기</p>
+    <div class="result_box"><div class="bluebox_result fl_left"><img src="<?=$job_imgurl?>"/></div>
+    <div class="fl_left">
+      <div class="re_text">당신의 내일(Work) 테스트 결과입니다.</div>
+      <div class="resulttwotext" style="width:460px;height:230px">
+        <p class="big">"<?=$user_job['job'];?>"</p>
+        <p class="sm"><?=$user_job['job_explain'];?></p>
+      </div>
+    </div>
+  </div>
+  <div class="undertext">세상에는 이처럼 당신이 하고 있는 일, 상상치도 못했던 다양한 내 일(Work) 이 많이 있답니다.<br/>
 빈곤한 환경으로 이런 일이 있다는 것 조차 모른 채 제한된 꿈을 꾸는 아이들을 도와주세요.<br/>
 당신의 내일(Work) 결과를 SNS에 공유하시면, 아이들의 내일(Tomorrow)을 위한 기부로 이어집니다.</div>
 <?
 	if ($user_info['media'] == $_gl['login_media']['facebook'])
 	{
 ?>
-	<div class="facebookbut"><a href="#" onclick="fb_share('<?=$user_job['job']?>','<?=$user_job['job_explain']?>','<?=$test_idx?>','<?=$_REQUEST['job']?>');return false;"><img src="images/facebook_sub.png"/></a></div>
+  <div class="facebookbut"><a href="#" onclick="fb_share('<?=$user_job['job']?>','<?=$user_job['job_explain']?>','<?=$test_idx?>','<?=$_REQUEST['job']?>');return false;"><img src="images/facebook_sub.png"/></a></div>
 <?
 	}else{
 ?>
-	<div class="facebookbut"><a href="#" onclick="ks_share('<?=$user_job['job']?>','<?=$user_job['job_explain']?>','<?=$test_idx?>','<?=$job_imgurl_kakao?>');return false;"><img src="images/ks_sub.png"/></a></div>
+  <div class="facebookbut"><a href="#" onclick="ks_share('<?=$user_job['job']?>','<?=$user_job['job_explain']?>','<?=$test_idx?>','<?=$job_imgurl_kakao?>');return false;"><img src="images/ks_sub.png"/></a></div>
 <?
 	}
 ?>
