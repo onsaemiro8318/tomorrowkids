3c6d5d328578132d8b24e63bd259d80a3336e98a
kyhfan <yh.kim@minivertising.kr>
2014-11-04 13:17:04 +0900

    테스트 답변 선택 효과 주기8

--- js/tk.js
+++ js/tk.js
@@ -659,10 +659,10 @@ function go_next_question2(idx,num, selected_val)
 	if (idx_gubun > 0)
 	{
 		$('#answer_al_2').fadeOut(1000,function(){$('#answer_al_2').css("visibility","hidden‌​").css("display","");});
-		$('#answer2').fadeOut(1000,function(){$('#answer2').css("visibility","hidden‌​").css("display","");});
+		$('#answer2').fadeOut(1000,function(){$('#answer2').css("visibility","hidden‌​").css("display","");}).complete(go_next_sub(idx,num, selected_val););
 	}else{
 		$('#answer_al_1').fadeOut(1000,function(){$('#answer_al_1').css("visibility","hidden‌​").css("display","");});
-		$('#answer1').fadeOut(1000,function(){$('#answer1').css("visibility","hidden‌​").css("display","");});
+		$('#answer1').fadeOut(1000,function(){$('#answer1').css("visibility","hidden‌​").css("display","");}).complete(go_next_sub(idx,num, selected_val););
 	}
 	/*
 	var sel_val = idx;
@@ -697,6 +697,38 @@ function go_next_question2(idx,num, selected_val)
 	*/
 }
 
+function go_next_sub(){
+	var sel_val = idx;
+	var gubun   = "";
+	if (sel_val == "")
+	{
+		alert('하나의 답변을 꼭 선택해 주세요.');
+		if (num == 2)
+			location.href="work_test.php";
+		return false;
+	}
+
+	if (num > 10)
+	{
+		flag_num = flag_num + 1;
+
+		if (flag_num > 1)
+		{
+			return false;
+		}
+
+		$(".mask").fadeTo(1000,0.7);
+	}
+  
+	if (selected_val == "")
+		gubun = "";
+	else
+		gubun = "|";
+
+	sel_val = selected_val + gubun + sel_val;
+	go_test(num, sel_val);
+
+}
 // 이메일 입력
 function input_email(val)
 {
