e1a687402c1a10dc028e19309aa5f4492dfeae57
kyhfan <yh.kim@minivertising.kr>
2014-11-05 19:10:21 +0900

    이미지 사이즈 수정1

--- js/tk.js
+++ js/tk.js
@@ -423,7 +423,7 @@ window.fbAsyncInit = function() {
 		cookie     : true,  // enable cookies to allow the server to access 
 						// the session
 		xfbml      : true,  // parse social plugins on this page
-		version    : 'v2.1' // use version 2.1
+		version    : 'v2.2' // use version 2.1
 	});
 
 	FB.getLoginStatus(function(response) {
