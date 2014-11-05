9ba1a67a71203cf4cc9d6bbe366358e891c44645
kyhfan <yh.kim@minivertising.kr>
2014-11-05 17:43:04 +0900

    모바일 속도 개선3

--- config.php
+++ config.php
@@ -5,8 +5,6 @@
 	include_once "include/global.php"; 			//변수정보
 	include_once "include/function.php"; 		//함수정보
 	include_once "include/dbi.php"; 			//DB 연결정보
-	include_once "include/page.class.php"; 		//페이지 클래스
-	include_once "include/class.image.php"; 		//이미지 처리 클래스
 
 	mysqli_query ($my_db,"set names utf8");
 
