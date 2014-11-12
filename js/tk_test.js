ec30c058709ee47231e87499d07551012968c565
kyhfan <yh.kim@minivertising.kr>
2014-11-12 18:19:46 +0900

    사이트 로직 변경19

--- js/tk_test.js
+++ js/tk_test.js
@@ -14,6 +14,24 @@ var browser = {
 };   
 
 
+window.fbAsyncInit = function() {
+	FB.init({
+		appId      : '293604627507652',
+		xfbml      : true,  // parse social plugins on this page
+		version    : 'v2.2' // use version 2.1
+	});
+};
+
+// Load the SDK asynchronously
+(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/ko_KR/sdk.js";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
 
 /********************** 이메일 입력 **********************/
 function close_email(){
@@ -425,30 +443,6 @@ function checkLoginState() {
 		statusChangeCallback(response);
 	});
 }
-
-window.fbAsyncInit = function() {
-	FB.init({
-		appId      : '293604627507652',
-		cookie     : true,  // enable cookies to allow the server to access 
-						// the session
-		xfbml      : true,  // parse social plugins on this page
-		version    : 'v2.0' // use version 2.1
-	});
-
-	FB.getLoginStatus(function(response) {
-		statusChangeCallback(response);
-	});
-
-};
-
-// Load the SDK asynchronously
-(function(d, s, id) {
-	var js, fjs = d.getElementsByTagName(s)[0];
-	if (d.getElementById(id)) return;
-	js = d.createElement(s); js.id = id;
-	js.src = "//connect.facebook.net/ko_KR/sdk.js";
-	fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
 
 // Here we run a very simple test of the Graph API after login is
 // successful.  See statusChangeCallback() for when this call is made.
