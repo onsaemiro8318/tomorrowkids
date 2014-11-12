11b00728a32984629a2c1e31a91a1b291e817976
kyhfan <yh.kim@minivertising.kr>
2014-11-12 21:45:14 +0900

    사이트 로직 변경31

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
     
 	//환경설정 파일
 	include_once "include/global.php"; 			//변수정보
-	include_once "include/function.php"; 		//함수정보
+	include_once "include/function_test.php"; 		//함수정보
 	include_once "include/dbi.php"; 			//DB 연결정보
 
 	mysqli_query ($my_db,"set names utf8");
