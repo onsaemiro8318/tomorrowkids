fa3674deace7c161a3bcd71cdcfd2df17f397bca
kyhfan <yh.kim@minivertising.kr>
2014-11-25 16:53:14 +0900

    페북 오류 수정9

--- js/tk.js
+++ js/tk.js
@@ -14,6 +14,23 @@ var browser = {
 };   
 
 
+window.fbAsyncInit = function() {
+	FB.init({
+		appId      : '293604627507652',
+		xfbml      : true,  // parse social plugins on this page
+		version    : 'v2.2' // use version 2.1
+	});
+};
+
+// Load the SDK asynchronously
+(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/en_US/sdk.js";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
 
 
 /********************** 이메일 입력 **********************/
@@ -357,7 +374,6 @@ function ks_share(job, job_explain, test_idx, job_imgurl)
 
 function fb_share(job, job_explain, test_idx, job_num)
 {
-
 	FB.ui(
 	{
 		method: 'feed',
