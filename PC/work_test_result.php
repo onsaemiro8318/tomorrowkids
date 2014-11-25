97d1f3f3778a6831db17a5486e6ea1ae2eadde0a
kyhfan <yh.kim@minivertising.kr>
2014-11-25 16:46:51 +0900

    페북 오류 수정7

--- PC/work_test_result.php
+++ PC/work_test_result.php
@@ -26,22 +26,6 @@
 	$job_imgurl_kakao	= $_gl['kakao_img'][$_REQUEST['job']];
 ?>
 <script type="text/javascript">
-window.fbAsyncInit = function() {
-	FB.init({
-		appId      : '293604627507652',
-		xfbml      : true,  // parse social plugins on this page
-		version    : 'v2.2' // use version 2.1
-	});
-};
-
-// Load the SDK asynchronously
-(function(d, s, id) {
-	var js, fjs = d.getElementsByTagName(s)[0];
-	if (d.getElementById(id)) return;
-	js = d.createElement(s); js.id = id;
-	js.src = "//connect.facebook.net/en_US/sdk.js";
-	fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
 
 
 
