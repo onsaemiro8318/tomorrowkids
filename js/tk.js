ee4bf9bc2ac9124431a5f29a85191f3460cec8a5
kyhfan <yh.kim@minivertising.kr>
2014-11-25 16:49:16 +0900

    페북 오류 수정8

--- js/tk.js
+++ js/tk.js
@@ -357,22 +357,6 @@ function ks_share(job, job_explain, test_idx, job_imgurl)
 
 function fb_share(job, job_explain, test_idx, job_num)
 {
-window.fbAsyncInit = function() {
-	FB.init({
-		appId      : '293604627507652',
-		xfbml      : true,  // parse social plugins on this page
-		version    : 'v2.2' // use version 2.1
-	});
-};
-
-// Load the SDK asynchronously
-(function(d, s, id) {
-	var js, fjs = d.getElementsByTagName(s)[0];
-	if (d.getElementById(id)) return;
-	js = d.createElement(s); js.id = id;
-	js.src = "//connect.facebook.net/en_US/sdk.js";
-	fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
 
 	FB.ui(
 	{
