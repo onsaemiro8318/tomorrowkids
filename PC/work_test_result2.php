bbaa279bf512353230baf2a0dcfb1ddfcb839a06
kyhfan <yh.kim@minivertising.kr>
2014-11-26 12:33:58 +0900

    페북 공유 수정26

--- PC/work_test_result2.php
+++ PC/work_test_result2.php
@@ -218,7 +218,7 @@ function fb_share2()
 	var sTop=window.screen.height/2-(280);
 	var sLeft=window.screen.width/2-(310);
 
-	var newWindow = window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent('http://www.tomorrowkids.or.kr/PC/work_test_result2.php?sns_gubun=fb'),'sharer','toolbar=0,status=0,width=600,height=325,top='+sTop+',left='+sLeft);
+	var newWindow = window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent('http://www.tomorrowkids.or.kr/PC/work_test_result2.php'),'sharer','toolbar=0,status=0,width=600,height=325,top='+sTop+',left='+sLeft);
 
 }
 /*
