9ad46aeede7c83a526e1280da7e0d85753f22ba1
onsaemiro8318 <minivertising@minivertisingui-MacBook-Air.local>
2014-12-02 13:50:31 +0900

    pc index meta tag 직업 추가

--- PC/work_test_result_34.php
+++ PC/work_test_result_34.php
@@ -34,7 +34,7 @@
     <meta property="og:type" content="website" />
     <meta property="og:url" content="http://www.tomorrowkids.or.kr/PC/work_test_result_34.php" />
     <meta property="og:image" content="http://www.tomorrowkids.or.kr/images/fb/jobbigimg_34.jpg" />
-    <meta property="og:description" content="어릴 적 당신이 지녔던 '그 마음'을 팝니다">
+    <meta property="og:description" content="당신은 내일을 꿈꾸며 살아가고 있습니다. 당신에게 어울리는 직업을 찾아보세요!">
     <link rel="shortcut icon" type="image/x-icon" href="./images/tomorrow.ico" />
     <link rel="stylesheet" type="text/css" href="./css/style.css"/>
     <script src="https://developers.kakao.com/sdk/js/kakao.min.js"></script>
