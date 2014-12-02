2a1c0f1ee1d6e31051bc5a079ee6889c42c3157a
kyhfan <yh.kim@minivertising.kr>
2014-12-02 10:37:45 +0900

    페이스북 공유 수정6

--- index.php
+++ index.php
@@ -3,16 +3,20 @@
 
 
 	unset($media);
-	$media = $_REQUEST[media];
+	$media		= $_REQUEST['media'];
+	$job_idx	= $_REQUEST['job_idx'];
 
 	TK_InsertTrackingInfo($media, $gubun);
 
+	if ( isset($job_idx) == false)
+		$job_idx = "1";
+
 	if($check_mobile)
 	{
-		Header("Location:http://www.tomorrowkids.or.kr/MOBILE/");
+		Header("Location:http://www.tomorrowkids.or.kr/MOBILE/index_".$job_idx.".php");
 		exit;
 	}else{
-		Header("Location:http://www.tomorrowkids.or.kr/PC/");
+		Header("Location:http://www.tomorrowkids.or.kr/PC/index_".$job_idx.".php");
 		exit;
 	}
 
