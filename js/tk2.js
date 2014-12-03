97ba98bc39111f6d756f82a4f166ba35a89c1458
kyhfan <yh.kim@minivertising.kr>
2014-12-03 18:22:31 +0900

    카스 공유 방식 변경7

--- js/tk2.js
+++ js/tk2.js
@@ -92,7 +92,7 @@ function play_movie(gubun){
 function ks_share_mobile(job, job_explain, test_idx, job_imgurl)
 {
 	kakao.link("story").send({
-		post : "http://www.dreamfull.or.kr/tomorrowkids/ks",
+		post : " ",
 		appid : "http://www.dreamfull.or.kr/tomorrowkids/ks",
 		appver : "1.0",
 		appname : "내일을 부탁해",
