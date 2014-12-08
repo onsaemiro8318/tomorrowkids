37f2d50da4dc8d5f660797ff9eec3f648925eac3
onsaemiro8318 <minivertising@minivertisingui-MacBook-Air.local>
2014-12-08 11:20:13 +0900

    admin 공유매체  수정

--- admin/share_media_count.php
+++ admin/share_media_count.php
@@ -34,18 +34,46 @@
 
 		while ($media_data = mysqli_fetch_array($media_res))
 		{
+      $pc_sum = 0;
+      $mob_sum = 0;
+      
 			$pc_query		= "SELECT * FROM ".$_gl['tk_test_result_table']." WHERE media='".$media_data['media']."' AND gubun='PC' AND share='Y'";
 			$pc_count		= mysqli_num_rows(mysqli_query($my_db, $pc_query));
+      if($media_data['media'] == "facebook"){
+  			$pc_fb_query		= "SELECT * FROM ".$_gl['tk_test_result_table']." WHERE gubun='PC' AND fb_share='Y'";
+  			$pc_fb_count		= mysqli_num_rows(mysqli_query($my_db, $pc_fb_query));        
+        $pc_sum = $pc_count + $pc_fb_count;
+      }else {
+  			$pc_kt_query		= "SELECT * FROM ".$_gl['tk_test_result_table']." WHERE gubun='PC' AND kt_share='Y'";         			$pc_kt_count		= mysqli_num_rows(mysqli_query($my_db, $pc_kt_query));
+  			$pc_ks_query		= "SELECT * FROM ".$_gl['tk_test_result_table']." WHERE gubun='PC' AND ks_share='Y'";  
+  			$pc_ks_count		= mysqli_num_rows(mysqli_query($my_db, $pc_ks_query));
+        $pc_sum = $pc_count + $pc_kt_count + $pc_ks_count;
+      }
+
 			$mobile_query	= "SELECT * FROM ".$_gl['tk_test_result_table']." WHERE media='".$media_data['media']."' AND gubun='MOBILE' AND share='Y'";
 			$mobile_count	= mysqli_num_rows(mysqli_query($my_db, $mobile_query));
+      if($media_data['media'] == "facebook"){
+  			$mob_fb_query		= "SELECT * FROM ".$_gl['tk_test_result_table']." WHERE gubun='MOBILE' AND fb_share='Y'";
+  			$mob_fb_count		= mysqli_num_rows(mysqli_query($my_db, $mob_fb_query));
+        $mob_sum = $mobile_count + $mob_fb_count;                
+      }else{
+  			$mob_kt_query		= "SELECT * FROM ".$_gl['tk_test_result_table']." WHERE gubun='MOBILE' AND kt_share='Y'"; 
+  			$mob_kt_count		= mysqli_num_rows(mysqli_query($my_db, $mob_kt_query));
+        $mob_ks_query		= "SELECT * FROM ".$_gl['tk_test_result_table']." WHERE gubun='MOBILE' AND ks_share='Y'"; 
+  			$mob_ks_count		= mysqli_num_rows(mysqli_query($my_db, $mob_ks_query));        
+        $mob_sum = $mobile_count + $mob_kt_count + $mob_ks_count;
+      }
+
+            
 			$total_count		= $pc_count + $mobile_count;
+      $total = $pc_sum + $mob_sum;
 
 ?>
                   <tr>
                     <td><?=$media_data['media']?></td>
-                    <td><?=number_format($pc_count)?></td>
-                    <td><?=number_format($mobile_count)?></td>
-                    <td><?=number_format($total_count)?></td>
+                    <td><?=number_format($pc_sum)?></td>
+                    <td><?=number_format($mob_sum)?></td>
+                    <td><?=number_format($total)?></td>
                   </tr>
 <?php
         }
